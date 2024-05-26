c4ce9865ca75c73e83db5e8d4003bd66205ac1cd
phundanghoc <145929031+Phun1234@users.noreply.github.com>
2024-05-26 17:48:45 +0700

    update

--- src/components/core/layouts/MainLayout/index.tsx
+++ src/components/core/layouts/MainLayout/index.tsx
@@ -18,7 +18,7 @@ interface LayoutProps {
   isGuestPage?: boolean;
 }
 
-function Layout({ children, isGuestPage = false }: LayoutProps) {
+function Layout({ children, isGuestPage = true }: LayoutProps) {
   return (
     <S.LayoutWrapper>
       <S.Header>

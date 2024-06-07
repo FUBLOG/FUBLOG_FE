a26c358b4fb679778f05fa046c330b0253178312
Saugao274 <thuynttde170035@fpt.edu.vn>
2024-06-07 08:14:37 +0700

    icon

--- src/components/core/layouts/MainLayout/index.tsx
+++ src/components/core/layouts/MainLayout/index.tsx
@@ -2,6 +2,9 @@
 import { useState, ReactNode } from "react";
 import { Flex, Modal, Input } from "antd";
 import Image from "next/image";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Link from "next/link";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import {
   HomeOutlined,
   SearchOutlined,
@@ -12,11 +15,17 @@ import {
   CaretDownOutlined,
   PictureOutlined,
   SendOutlined,
+  MessageFilled,
   CloseOutlined,
+  HomeFilled,
+  EditFilled,
+  BellFilled,
 } from "@ant-design/icons";
 import Button from "../../common/Button";
 import logo from "@/public/logo.png";
+
 import * as S from "./styles";
+
 interface MessageModalProps {
   visible: boolean;
   onClose: () => void;
@@ -83,14 +92,13 @@ const MessageModal: React.FC<MessageModalProps> = ({ visible, onClose }) => {
 
   return (
     <Modal
-    open={visible}
-    onCancel={onClose}
-    footer={null}
-    width={800}
-    styles={{ body: { backgroundColor: "#FAF0E6", padding: 0 } }}
-    closable={false}
-  >
-  
+      open={visible}
+      onCancel={onClose}
+      footer={null}
+      width={800}
+      styles={{ body: { backgroundColor: "#FAF0E6", padding: 0 } }}
+      closable={false}
+    >
       <S.ModalContainer>
         <S.CloseButton onClick={onClose}>
           <CloseOutlined />
@@ -220,20 +228,21 @@ interface LayoutProps {
   readonly children: ReactNode;
   readonly isGuestPage?: boolean;
 }
-interface LayoutProps {
-  readonly children: ReactNode;
-  readonly isGuestPage?: boolean;
-}
 
 function MainLayout({ children, isGuestPage = true }: LayoutProps) {
-  const [showMessageModal, setShowMessageModal] = useState(false); // Trạng thái modal nhắn tin
-
+  const [showMessageModal, setShowMessageModal] = useState(false);
+  const [nav, setNav] = useState("home");
   const handleOpenMessageModal = () => {
     setShowMessageModal(true);
+    setNav("mess");
   };
 
   const handleCloseMessageModal = () => {
     setShowMessageModal(false);
+  };
+
+  const handleSetNavigation = (e: string) => {
+    setNav(e);
   };
 
   return (
@@ -243,35 +252,67 @@ function MainLayout({ children, isGuestPage = true }: LayoutProps) {
         <S.Container>
           <Image src={logo} alt="logo header" />
           <S.IconContainer>
-            <a href="/home">
-              <HomeOutlined style={{ fontSize: "22px" }} />
-            </a>
-            <SearchOutlined style={{ fontSize: "22px" }} />
-            <EditOutlined style={{ fontSize: "22px" }} />
-            <MessageOutlined
-              style={{ fontSize: "22px" }}
-              onClick={handleOpenMessageModal}
-            />
-            <BellOutlined style={{ fontSize: "22px" }} />
+            <Link href="/home" onClick={(e) => handleSetNavigation("home")}>
+              {nav === "home" ? (
+                <HomeFilled style={{ fontSize: "22px" }} />
+              ) : (
+                <HomeOutlined style={{ fontSize: "22px" }} />
+              )}
+            </Link>
+            <Link href="" onClick={() => handleSetNavigation("search")}>
+              {nav === "search" ? (
+                <FontAwesomeIcon
+                  style={{ fontSize: "22px" }}
+                  icon={faMagnifyingGlass}
+                />
+              ) : (
+                <SearchOutlined
+                  style={{
+                    fontSize: "22px",
+                  }}
+                />
+              )}
+            </Link>
+            <Link href="" onClick={() => handleSetNavigation("edit")}>
+              {nav === "edit" ? (
+                <EditFilled style={{ fontSize: "22px" }} />
+              ) : (
+                <EditOutlined style={{ fontSize: "22px" }} />
+              )}
+            </Link>
+            <Button type="text" onClick={handleOpenMessageModal}>
+              {nav === "mess" ? (
+                <MessageFilled style={{ fontSize: "22px" }} />
+              ) : (
+                <MessageOutlined style={{ fontSize: "22px" }} />
+              )}
+            </Button>
+            <Button type="text" onClick={() => handleSetNavigation("bell")}>
+              {nav === "bell" ? (
+                <BellFilled style={{ fontSize: "22px" }} />
+              ) : (
+                <BellOutlined style={{ fontSize: "22px" }} />
+              )}
+            </Button>
           </S.IconContainer>
           {isGuestPage ? (
             <Flex gap={15} style={{ marginRight: "20px" }}>
-              <a href="/sign-in">
+              <Link href="/sign-in">
                 <Button type="default" $width="100px">
                   Đăng nhập
                 </Button>
-              </a>
-              <a href="/sign-up">
+              </Link>
+              <Link href="/sign-up">
                 <Button color="red" type="primary" $width="100px">
                   Đăng ký
                 </Button>
-              </a>
+              </Link>
             </Flex>
           ) : (
             <S.UserIconContainer>
-              <a href="/profile">
+              <Link href="/profile">
                 <UserOutlined style={{ fontSize: "28px" }} />
-              </a>
+              </Link>
               <CaretDownOutlined
                 style={{ fontSize: "18px", marginLeft: "4px" }}
               />

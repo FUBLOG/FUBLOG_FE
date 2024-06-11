85230dbe5b672dfa6d78bafb386578a16426fbaa
Saugao274 <thuynttde170035@fpt.edu.vn>
2024-06-11 14:21:41 +0700

    sonar

--- src/components/core/layouts/MainLayout/index.tsx
+++ src/components/core/layouts/MainLayout/index.tsx
@@ -72,9 +72,12 @@ function MainLayout({ children }: LayoutProps) {
       return false;
     };
     const isValid = isValidUser();
-    isValid
-      ? (setIsGuest(!isValid), webStorageClient.set(constants.IS_AUTH, true))
-      : webStorageClient.set(constants.IS_AUTH, false);
+    if (isValid) {
+      setIsGuest(!isValid);
+      webStorageClient.set(constants.IS_AUTH, true);
+    } else {
+      webStorageClient.set(constants.IS_AUTH, false);
+    }
   }, []);
   return (
     <S.LayoutWrapper>

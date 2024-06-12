10ab1535ebf040e350e19856d7057868af58685a
Saugao274 <thuynttde170035@fpt.edu.vn>
2024-06-12 11:35:30 +0700

    update

--- src/components/modules/SearchBar/Main/index.tsx
+++ src/components/modules/SearchBar/Main/index.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React from "react";
 import { InputWrapper, SearchIcon, StyledInput } from "./style";
 import SearchInfo from "../SearchInfo";
 

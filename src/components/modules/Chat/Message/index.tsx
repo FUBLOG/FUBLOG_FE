1e0b0048120698d8abd70599197f410e1e0595cd
phundanghoc <145929031+Phun1234@users.noreply.github.com>
2024-06-16 15:10:37 +0700

    up

--- src/components/modules/Chat/Message/index.tsx
+++ src/components/modules/Chat/Message/index.tsx
@@ -6,34 +6,34 @@ import MessageHeader from "./MessageHeader";
 import InputMessage from "./InputMessage";
 
 const Message = () => {
-const { selectedConversation, setSelectedConversation } = useConversation();
-const messagesEndRef = useRef<HTMLDivElement>(null);
-useEffect(() => {
-messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
-return () => setSelectedConversation(null);
-}, [setSelectedConversation]);
+  const { selectedConversation, setSelectedConversation } = useConversation();
+  const messagesEndRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+    return () => setSelectedConversation(null);
+  }, [setSelectedConversation]);
 
-return (
-<>
-{!selectedConversation ? (
-<NoChatSelected />
-) : (
-<S.ChatContainer>
-<MessageHeader />
-<MessageList />
-<InputMessage />
-</S.ChatContainer>
-)}
-</>
-);
+  return (
+    <>
+      {!selectedConversation ? (
+        <NoChatSelected />
+      ) : (
+        <S.ChatContainer>
+          <MessageHeader />
+          <MessageList />
+          <InputMessage />
+        </S.ChatContainer>
+      )}
+    </>
+  );
 };
 const NoChatSelected = () => {
-return (
-<S.NoChatSelected>
-<S.NoChatSelectedText>
-Chọn một cuộc trò chuyện để bắt đầu nào!
-</S.NoChatSelectedText>
-</S.NoChatSelected>
-);
+  return (
+    <S.NoChatSelected>
+      <S.NoChatSelectedText>
+        Chọn một cuộc trò chuyện để bắt đầu nào!
+      </S.NoChatSelectedText>
+    </S.NoChatSelected>
+  );
 };
 export default Message;

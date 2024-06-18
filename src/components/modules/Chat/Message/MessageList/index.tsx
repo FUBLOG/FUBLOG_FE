5d6a7ab8f47af39e8bbd800cc22a64d5c5fa43f5
phundanghoc <145929031+Phun1234@users.noreply.github.com>
2024-06-18 18:51:39 +0700

    dcdc

--- src/components/modules/Chat/Message/MessageList/index.tsx
+++ src/components/modules/Chat/Message/MessageList/index.tsx
@@ -1,29 +1,47 @@
+// MessageList.tsx
+import React, { useEffect, useRef } from 'react';
 import { useGetMessage } from "@/hooks/useMessage";
-import * as S from "../../styles";
-import { useEffect, useRef } from "react";
 import { useAuthContext } from "@/contexts/AuthContext";
 import { useListenMessage } from "@/hooks/useListen";
 import { extractTime } from "@/utils";
+import {
+  MessagesList,
+  ChatContainer,
+  ChatHeader,
+  ChatTime,
+  ChatBubble,
+  ChatFooter
+} from "../../styles";
+
 const MessageList = () => {
   const { messages } = useGetMessage();
   const { userInfo } = useAuthContext();
   useListenMessage();
   const messagesEndRef = useRef<HTMLDivElement>(null);
+
   useEffect(() => {
     messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
-  });
+  }, [messages]);
+
   return (
-    <S.MessagesList>
-      {messages.map((message) => (
-        <S.MessageItem key={message?._id} isOwnMessage={message.senderId === userInfo?.userId}>
-          <S.MessageContent isOwnMessage={message.senderId === userInfo?.userId}>
-            {message.message}
-          </S.MessageContent>
-          <S.MessageTime>{extractTime(message?.createdAt)}</S.MessageTime>
-        </S.MessageItem>
-      ))}
+    <MessagesList>
+      {messages.map((message) => {
+        const isOwnMessage = message.senderId === userInfo?.userId;
+        return (
+          <ChatContainer key={message?._id}>
+            <ChatHeader>
+              {isOwnMessage ? userInfo?.profileHash : message.senderName}
+              <ChatTime>{extractTime(message?.createdAt)}</ChatTime>
+            </ChatHeader>
+            <ChatBubble isOwnMessage={isOwnMessage}>{message.message}</ChatBubble>
+            <ChatFooter isOwnMessage={isOwnMessage}>
+              {isOwnMessage ? 'Seen' : 'Delivered'}
+            </ChatFooter>
+          </ChatContainer>
+        );
+      })}
       <div ref={messagesEndRef} />
-    </S.MessagesList>
+    </MessagesList>
   );
 };
 

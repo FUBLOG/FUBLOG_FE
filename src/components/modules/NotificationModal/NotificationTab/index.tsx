063ed0721b9e244d949a0b6df6fc9a7d82c1b9bd
Saugao274 <thuynttde170035@fpt.edu.vn>
2024-06-25 22:29:42 +0700

    a

--- src/components/modules/NotificationModal/NotificationTab/index.tsx
+++ src/components/modules/NotificationModal/NotificationTab/index.tsx
@@ -1,36 +1,34 @@
-import { List, Avatar, Skeleton } from 'antd';
-import { fromNow } from '@/utils';
-import { useGetNotification } from '@/hooks/useNotification';
-import Link from 'next/link';
+import { List, Avatar, Skeleton } from "antd";
+import { fromNow } from "@/utils";
+import { useGetNotification } from "@/hooks/useNotification";
+import Link from "next/link";
 
-const NotificationTab = ({onclose}:any) => {
-    const { loading, notifications } = useGetNotification();
-    const handleClick = () => {
-        onclose()
-    }
-    return loading ? (<Loading />) : (
-        <List
-            itemLayout="horizontal"
-            dataSource={notifications}
-            renderItem={(item: any) => (
-                <Link href={item?.link} onClick={handleClick} >
-                    <List.Item
-                        key={item.id}
-                        className="notification-item"
-
-
-                    >
-                        <List.Item.Meta
-                            avatar={<Avatar src={item?.image[0]} />}
-                            title={<span>{item.title}</span>}
-                            description={<span>{fromNow(new Date(item.createdAt))}</span>}
-                        />
-                    </List.Item>
-                </Link >
-            )}
-        />)
-}
+const NotificationTab = ({ onclose }: any) => {
+  const { loading, notifications } = useGetNotification();
+  const handleClick = () => {
+    onclose();
+  };
+  return loading ? (
+    <Loading />
+  ) : (
+    <List
+      itemLayout="horizontal"
+      dataSource={notifications}
+      renderItem={(item: any) => (
+        <Link href={item?.link} onClick={handleClick}>
+          <List.Item key={item.id} className="notification-item">
+            <List.Item.Meta
+              avatar={<Avatar src={item?.image[0]} />}
+              title={<span>{item.title}</span>}
+              description={<span>{fromNow(new Date(item.createdAt))}</span>}
+            />
+          </List.Item>
+        </Link>
+      )}
+    />
+  );
+};
 const Loading = () => {
-    return <Skeleton active round avatar title />;
-}
+  return <Skeleton active round avatar title />;
+};
 export default NotificationTab;

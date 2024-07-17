41af3280ac3ca3e61669d2aad7237d8e18227018
Thanh Thủy <94378475+Saugao274@users.noreply.github.com>
2024-07-17 21:21:49 +0700

    intro (#61)
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update not to ovtk
    
    * update not to ovtk

--- src/components/modules/SignIn/Main/styles.ts
+++ src/components/modules/SignIn/Main/styles.ts
@@ -3,8 +3,8 @@ import styled from "styled-components";
 
 export const HomeWrapper = styled(Flex)`
   flex-direction: row;
-  justify-content: space-around;
-  align-items: center;
+  justify-content: center;
+  align-items: flex-start;
   gap: 20px;
   span {
     color: #b9b4c7 !important;

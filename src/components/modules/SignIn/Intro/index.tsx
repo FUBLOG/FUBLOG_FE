d4701c9dda11fa3ce84248fddedf46d47bc6e1b4
Saugao274 <thuynttde170035@fpt.edu.vn>
2024-07-17 13:23:58 +0700

    update sương mù

--- src/components/modules/SignIn/Intro/index.tsx
+++ src/components/modules/SignIn/Intro/index.tsx
@@ -1,16 +1,45 @@
 "use client";
 import Image from "next/legacy/image";
-
 import introAu from "@/public/introAu.png";
-
 import * as S from "./styles";
+import { useEffect } from "react";
 
-function Intro() {
+const Intro: React.FC = () => {
+  useEffect(() => {
+    const handleMouseMove = (e: MouseEvent) => {
+      const x =
+        e.pageX ||
+        e.clientX +
+          (document.documentElement.scrollLeft || document.body.scrollLeft);
+      const y =
+        e.pageY ||
+        e.clientY +
+          (document.documentElement.scrollTop || document.body.scrollTop);
+
+      const overlay = document.querySelector(".overlay") as HTMLElement;
+      if (overlay) {
+        overlay.style.background = `radial-gradient(circle at ${x}px ${y}px, transparent, #f3e8d8)`;
+      }
+    };
+
+    const overlay = document.querySelector(".overlay") as HTMLElement;
+    if (overlay) {
+      overlay.style.background = `radial-gradient(circle at center, transparent, rgba(213, 237, 232, 0.797))`;
+    }
+
+    window.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
+
   return (
     <S.HomeWrapper>
+      <div className="overlay"></div>
       <Image src={introAu} alt="logo header" />
     </S.HomeWrapper>
   );
-}
+};
 
 export default Intro;
--- src/components/modules/SignIn/Intro/styles.ts
+++ src/components/modules/SignIn/Intro/styles.ts
@@ -2,8 +2,20 @@ import { Flex } from "antd";
 import styled from "styled-components";
 
 export const HomeWrapper = styled(Flex)`
-  padding-bottom: 50px;
-
-  flex-direction: row;
-  justify-content: space-evenly;
+  .overlay {
+    overflow: hidden;
+    background-color: black;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: radial-gradient(
+      circle at center,
+      transparent,
+      rgba(0, 0, 0, 0)
+    );
+    pointer-events: none;
+    z-index: 10000;
+  }
 `;

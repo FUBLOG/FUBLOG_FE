7ef6542444d7e2df8bbce10ec0592cade82580ec
Saugao274 <thuynttde170035@fpt.edu.vn>
2024-07-17 19:04:37 +0700

    update

--- src/components/modules/SignIn/Intro/index.tsx
+++ src/components/modules/SignIn/Intro/index.tsx
@@ -1,16 +1,68 @@
 "use client";
 import Image from "next/legacy/image";
-
-import introAu from "@/public/introAu.png";
-
+import main from "@/public/introAu.png";
+import screenshot1 from "@/public/slider/screenshot1.png";
+import screenshot2 from "@/public/slider/screenshot2.png";
+import screenshot3 from "@/public/slider/screenshot4.png"; // Fixed to screenshot3.png
 import * as S from "./styles";
+import React from "react";
+import { Carousel } from "antd";
 
-function Intro() {
+const Intro = () => {
   return (
     <S.HomeWrapper>
-      <Image src={introAu} alt="logo header" />
+      <S.ImageWrapper>
+        <Image
+          src={main}
+          alt="Screenshot 1"
+          height={550}
+          width={300}
+          objectFit="cover"
+          style={{
+            borderRadius: "20px",
+          }}
+        />
+      </S.ImageWrapper>
+      <Carousel autoplay effect="fade">
+        <S.ContentStyle>
+          <S.ImageSliderWrapper>
+            <Image
+              src={screenshot1}
+              alt="Screenshot 1"
+              className="post-image"
+              height={550}
+              width={300}
+              objectFit="cover"
+            />
+          </S.ImageSliderWrapper>
+        </S.ContentStyle>
+        <S.ContentStyle>
+          <S.ImageSliderWrapper>
+            <Image
+              src={screenshot2}
+              alt="Screenshot 2"
+              className="post-image"
+              height={550}
+              width={300}
+              objectFit="cover"
+            />
+          </S.ImageSliderWrapper>
+        </S.ContentStyle>
+        <S.ContentStyle>
+          <S.ImageSliderWrapper>
+            <Image
+              src={screenshot3}
+              alt="Screenshot 3"
+              className="post-image"
+              height={550}
+              width={300}
+              objectFit="cover"
+            />
+          </S.ImageSliderWrapper>
+        </S.ContentStyle>
+      </Carousel>
     </S.HomeWrapper>
   );
-}
+};
 
 export default Intro;
--- src/components/modules/SignIn/Intro/styles.ts
+++ src/components/modules/SignIn/Intro/styles.ts
@@ -1,9 +1,45 @@
-import { Flex } from "antd";
 import styled from "styled-components";
 
-export const HomeWrapper = styled(Flex)`
-  padding-bottom: 50px;
+export const HomeWrapper = styled.div`
+  width: 50%;
+  height: 100%;
+  position: relative;
+  .ant-carousel {
+    position: relative;
+    bottom: 520px;
+    left: 150px;
+  }
+  .ant-carousel .slick-slide {
+    position: relative;
+    text-align: center;
+    line-height: 450px;
+    overflow: hidden;
+  }
+`;
 
-  flex-direction: row;
-  justify-content: space-evenly;
+export const ContentStyle = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ImageWrapper = styled.div`
+  width: 300px;
+  height: 547px;
+  border: 3px solid #c0c0c0;
+  border-radius: 20px;
+  .post-image {
+    object-fit: cover;
+    border-radius: 20px;
+  }
+`;
+export const ImageSliderWrapper = styled.div`
+  width: 300px;
+  height: 547px;
+  border: 3px solid #ff000000;
+  border-radius: 20px;
+  .post-image {
+    object-fit: cover;
+    border-radius: 20px;
+  }
 `;

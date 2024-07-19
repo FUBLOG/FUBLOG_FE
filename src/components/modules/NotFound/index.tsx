752680ce7c77c543a31d6420d925c153636d8ee3
huynhmanh2003 <136096602+huynhmanh2003@users.noreply.github.com>
2024-07-19 19:25:10 +0700

    done fix

--- src/components/modules/NotFound/index.tsx
+++ src/components/modules/NotFound/index.tsx
@@ -1,34 +1,43 @@
-"use client";
-
+'use client'
 import React from "react";
 import * as S from "./styles";
 import Typography from "@/components/core/common/Typography";
 import Button from "@/components/core/common/Button";
 import Link from "next/link";
 import { useAuthContext } from "@/contexts/AuthContext";
+import useThemeStore from "@/hooks/useTheme";
 
 const NotFound: React.FC = () => {
   const { userInfo } = useAuthContext();
+  const darkMode = useThemeStore((state) => state.darkMode);
 
   return (
-    <S.HomeWrapper>
+    <S.HomeWrapper className={darkMode ? "theme-dark" : "theme-light"}>
       <S.Message>
-        <Typography variant="h5" fontSize="24px">
+        <Typography color="black" variant="h5" fontSize="24px">
           Trang này không hiển thị
         </Typography>
-        <Typography variant="body-text-small-normal" fontSize="16px">
+        <Typography
+          color="black"
+          variant="body-text-small-normal"
+          fontSize="16px"
+        >
           Có thể liên kết đã hỏng hoặc trang đã bị gỡ. Hãy kiểm tra xem liên kết
           mà bạn đang cố mở có chính xác không.
         </Typography>
         <S.ButtonContainer>
           <Link href={userInfo?._id ? "/" : "/sign-in"} passHref>
             <Button
-              type="primary"
-              $width="100px"
-              $padding="30px"
+              $color= "#000"
+              $hoverColor={darkMode ? "#000" : "#fff"}
+              $borderColor="#000"
+              $hoverBackgroundColor={darkMode ? "#F7D600" : "#000"}
+              $width="200px"
+              $padding="40px"
               $fontSize="16px"
+              $fontWeight="bold"
             >
-              Quay lại
+              Quay lại trang chủ
             </Button>
           </Link>
         </S.ButtonContainer>
